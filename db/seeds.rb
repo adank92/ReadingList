93dea608229fdd96512850a4d7ff17c411c0edec
Adan Koch <adank92@gmail.com>
2015-03-11 17:52:40 -0300

    Scaffold and several modifications

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create!([{
+    title: "Hyperion",
+    author: "Dan Simmons",
+    description: "Probable my favorite science fiction book",
+    amazon_id: "0553283685",
+    rating: 5,
+    finished_on: 10.days.ago
+  },
+  {
+    title: "Jony Ive: The Genius Behing Apple's Greatest Products",
+    author: "Leander Kahney",
+    description: "Even though I respect Ive, I felt this biography only hit skin deep.",
+    amazon_id: "159184617X",
+    rating: 4,
+    finished_on: 1.day.ago
+  }
+  ]
+)
